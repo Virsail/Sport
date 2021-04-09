e33a37b33bed9cdde5ef19ec51df541773b5e760
virsail <virsaileric@gmail.com>
2021-04-09 12:55:59 +0300

    add a prompt function on the web page

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,75 @@
-(((1+2)*3)/4)^5
-the integers numbers have type int ,.while those with a fractional part have type float
-the / will always return a float
-.on the division mathematically its a float ,.then also for the fact that you're using the aparatus ,.it'll return a float
-so definitely they'll be a type error bc you're mixing type int and type float ,.
-,.another way you can do it is use python eval()function,.it can accept arbitrary expressions like instead of 
-having the addition in its set of brackets then a bracket for the multiplication and division.,uneza kuwa na multiple operations 
-within the same bracket like(1+2-3*4)..in the spirit of code refactor ,.si you remember how we used to refactor??
-like unatoa what's not necessary so that your code runs with the least lnes possible
-
-
-floor division and get an int istead of a float ,.use // ,.this will discard the remainder in the sense that you only have the 
-answer as an int ,.no .remainder
+prompt("Are you ready for the ultimate Delani experience?");
+$(document).ready(function() {
+  $(".toggle").click(function() {
+    $("#shown-design").toggle();
+    $("#hidden-design").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".toggle1").click(function() {
+    $("#shown-development").toggle();
+    $("#hidden-development").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".toggle2").click(function() {
+    $("#shown-product").toggle();
+    $("#hidden-product").toggle();
+  });
+});
+ 
+$(document).ready(function() {
+  $(".cruiser").mouseover(function() {
+    $("#j1").show()
+  })
+  $(".cruiser").mouseleave(function(){
+    $("#j1").hide()
+  })
+  $(".cruiser1").mouseover(function() {
+    $("#j2").show()
+  })
+  $(".cruiser1").mouseleave(function(){
+    $("#j2").hide()
+  })
+  $(".cruiser2").mouseover(function() {
+    $("#j3").show()
+  })
+  $(".cruiser2").mouseleave(function(){
+    $("#j3").hide()
+  })
+  $(".cruiser3").mouseover(function() {
+    $("#j4").show()
+  })
+  $(".cruiser3").mouseleave(function(){
+    $("#j4").hide()
+  })
+  $(".cruiser4").mouseover(function() {
+    $("#j5").show()
+  })
+  $(".cruiser4").mouseleave(function(){
+    $("#j5").hide()
+  })
+  $(".cruiser5").mouseover(function() {
+    $("#j6").show()
+  })
+  $(".cruiser5").mouseleave(function(){
+    $("#j6").hide()
+  })
+  $(".cruiser6").mouseover(function() {
+    $("#j7").show()
+  })
+  $(".cruiser6").mouseleave(function(){
+    $("#j7").hide()
+  })
+  $(".cruiser7").mouseover(function() {
+    $("#j8").show()
+  })
+  $(".cruiser7").mouseleave(function(){
+    $("#j8").hide()
+  })
+})
+$(document).ready(function(){
+  $(".section6").submit(function(){
+    alert("WE have received your message ,thank you for reaching out to Delani studio");
+  });
+});
